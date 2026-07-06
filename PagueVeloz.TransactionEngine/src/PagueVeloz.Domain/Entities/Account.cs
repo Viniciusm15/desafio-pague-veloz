c2b083fa9b171fa28a2361880cb02479a3077e8e
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-07-06 19:15:47 -0300

    feat: add domain entity classes

---
+++ PagueVeloz.TransactionEngine/src/PagueVeloz.Domain/Entities/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace PagueVeloz.Domain.Entities;
+
+public class Account
+{
+    public Guid Id { get; private set; }
+    public Guid CustomerId { get; private set; }
+    public decimal AvailableBalance { get; private set; }
+
+    private Account(Guid customerId)
+    {
+        Id = Guid.NewGuid();
+        CustomerId = customerId;
+        AvailableBalance = 0m;
+    }
+
+    public static Account Open(Guid customerId)
+    {
+        return new Account(customerId);
+    }
+}
---
+++ PagueVeloz.TransactionEngine/src/PagueVeloz.Domain/Entities/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace PagueVeloz.Domain.Entities;
+
+public class Customer
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Document { get; private set; }
+
+    private Customer(string name, string document)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Document = document;
+    }
+
+    public static Customer Create(string name, string document)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Nome do cliente é obrigatório.");
+
+        if (string.IsNullOrWhiteSpace(document))
+            throw new ArgumentException("Documento do cliente é obrigatório.");
+
+        return new Customer(name, document);
+    }
+}
