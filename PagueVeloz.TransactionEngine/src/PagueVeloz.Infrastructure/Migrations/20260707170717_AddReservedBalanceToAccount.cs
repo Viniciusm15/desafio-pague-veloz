6de0ba32be9428533bef3212e6cdc20e7d1841fe
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-07-07 14:10:48 -0300

    feat: add ReservedBalance column to Account table

---
+++ PagueVeloz.TransactionEngine/src/PagueVeloz.Infrastructure/Migrations/20260707170717_AddReservedBalanceToAccount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PagueVeloz.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReservedBalanceToAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ReservedBalance",
+                table: "Accounts",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReservedBalance",
+                table: "Accounts");
+        }
+    }
+}
